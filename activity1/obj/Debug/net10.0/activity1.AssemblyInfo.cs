6636f03acc235effd9b7bf8078616f1ca266c7b2
JKVelayooo <josefvelayo@gmail.com>
2026-04-07 12:29:14 +0800

    Added confirmation prompt logic to Even/Odd checker

--- activity1/obj/Debug/net10.0/activity1.AssemblyInfo.cs
+++ activity1/obj/Debug/net10.0/activity1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("activity1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9623af06ea82218819ee02e92ec58bc5cf2f5940")]
 [assembly: System.Reflection.AssemblyProductAttribute("activity1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("activity1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
